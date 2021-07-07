5274fd773e635181dc946dcb9404461a8dd12782
Abubakar <abubakarabdihakim77@gmail.com>
2021-07-07 17:57:39 +0100

    module commits

---
+++ locator.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+#this class allows to easily define and find all the elements on the web page
+class MainPageLocators(object):
+    GO_BUTTON = (By.ID, "submit")
+
+class SearchResultsPageLocators(object):
+    pass
