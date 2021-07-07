4f76eb5d058eb085eb44511bd235c3c8cc416c45
Abubakar <abubakarabdihakim77@gmail.com>
2021-07-07 18:20:05 +0100

    module commits

---
+++ element.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.support.ui import WebDriverWait
+
+class BasePageElement(object):
+#The value is what is passed as search_text_element but the obj is the class it is defined in.
+    def __set__(self, obj, value):
+        driver = obj.driver
+        WebDriverWait(driver, 100).until(
+            lambda driver: driver.find_element_by_name(self.locator))
+        driver.find_element_by_name(self.locator).clear()
+        driver.find_element_by_name(self.locator).send_keys(value)
+
+    def __get__(self, obj, owner):
+        driver = obj.driver
+        WebDriverWait(driver, 100).until(
+            lambda driver: driver.find_element_by_name(self.locator))
+        element = driver.find_element_by_name(self.locator)
+        return element.get_atrribute("value")
